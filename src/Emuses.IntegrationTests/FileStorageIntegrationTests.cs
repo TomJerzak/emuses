d2ab54d02f887291073ecd6ad565c98ad1d81a25
teja-1010100 <tomasz.jerzak@gmail.com>
2017-12-18 00:03:14 +0100

    FileStorage: Create (open) session.

--- src/Emuses.IntegrationTests/FileStorageIntegrationTests.cs
+++ src/Emuses.IntegrationTests/FileStorageIntegrationTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using FluentAssertions;
 using Xunit;
 
@@ -6,16 +7,28 @@ namespace Emuses.IntegrationTests
 {
     public class FileStorageIntegrationTests
     {
+        private const string DirectoryPath = @"C:\Temp\Emuses";
+        private const string SessionFileExtension = ".ses";
+
         [Fact]
         public void create_file_after_open_new_session()
-        {
-            IStorage storage = new FileStorage();
+        {            
+            IStorage storage = new FileStorage(DirectoryPath + "\\");
             var session = new Session().Open(30, storage);
 
+            Directory.CreateDirectory(DirectoryPath);
             var sessionCreated = storage.Create(session);
+            
+            var fileStream = new FileStream(DirectoryPath + "\\" + session.GetSessionId() + SessionFileExtension, FileMode.Open);
+            string sessionIdLine;
+            using (var reader = new StreamReader(fileStream))
+            {
+                sessionIdLine = reader.ReadLine();
+            }
 
             sessionCreated.GetSessionId().Should().NotBeNullOrEmpty();
             sessionCreated.GetExpiredDate().Should().BeAfter(DateTime.Now.AddMinutes(25));
+            sessionIdLine.Should().Be($"SessionId:{session.GetSessionId()}");
         }
     }
 }
--- src/Emuses/FileStorage.cs
+++ src/Emuses/FileStorage.cs
@@ -1,7 +1,16 @@
-﻿namespace Emuses
+﻿using System.IO;
+
+namespace Emuses
 {
     public class FileStorage : IStorage
     {
+        private readonly string _directoryPath;
+        
+        public FileStorage(string directoryPath)
+        {
+            _directoryPath = directoryPath;
+        }
+
         public Session GetBySessionId(string sessionId)
         {
             throw new System.NotImplementedException();
@@ -9,7 +18,15 @@
 
         public Session Create(Session session)
         {
-            throw new System.NotImplementedException();
+            using (var file = new StreamWriter(File.Create(_directoryPath + session.GetSessionId() + ".ses")))
+            {
+                file.WriteLine($"SessionId:{session.GetSessionId()}");
+                file.WriteLine($"Version:{session.GetVersion()}");
+                file.WriteLine($"Minutes:{session.GetMinutes()}");
+                file.WriteLine($"ExpiredDate:{session.GetExpiredDate()}");
+            }
+
+            return session;
         }
 
         public Session Update(Session session)
