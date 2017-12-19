2de7e3060522562aabd0e77277fe930ae22e735e
teja-1010100 <tomasz.jerzak@gmail.com>
2017-12-20 00:14:23 +0100

    Create PostgresStorage.

---
+++ src/Emuses.IntegrationTests/PostgresStorageIntegrationTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Emuses.IntegrationTests
+{
+    public class PostgresStorageIntegrationTests
+    {
+        private const string ConnectionString = "Host=127.0.0.1;Username=emuses;Password=emuses;Database=emuses";
+
+        [Fact]
+        public void insert_to_db_after_open_new_session()
+        {
+            var sessionCreated = CreateSession(out var sessionIdLine);
+
+            sessionCreated.GetSessionId().Should().NotBeNullOrEmpty();
+            sessionCreated.GetExpiredDate().Should().BeAfter(DateTime.Now.AddMinutes(25));
+            //sessionIdLine.Should().Be($"SessionId:{sessionCreated.GetSessionId()}");
+        }
+
+        private static Session CreateSession(out string sessionIdLine)
+        {
+            IStorage storage = new PostgresStorage(ConnectionString);
+            var session = new Session(30, storage).Open();
+
+            sessionIdLine = ""; // storage.GetBySessionId(session.GetSessionId()).GetSessionId();
+
+            return session;
+        }
+    }
+}
---
+++ src/Emuses/PostgresStorage.cs
@@ -0,0 +1,75 @@
+﻿using Npgsql;
+
+namespace Emuses
+{
+    public class PostgresStorage : IStorage
+    {
+        private readonly string _connectionString;
+
+        private PostgresStorage()
+        {
+        }
+
+        public PostgresStorage(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public Session GetBySessionId(string sessionId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Session Create(Session session)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                using (var command = new NpgsqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "INSERT INTO sessions (session_id) VALUES (@session_id)";
+                    command.Parameters.AddWithValue("session_id", session.GetSessionId());
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            throw new System.NotImplementedException();
+        }
+
+        public Session Update(Session session)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(string sessionId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
+/*
+ var connString = "Host=myserver;Username=mylogin;Password=mypass;Database=mydatabase";
+
+using (var conn = new NpgsqlConnection(connString))
+{
+    conn.Open();
+
+    // Insert some data
+    using (var cmd = new NpgsqlCommand())
+    {
+        cmd.Connection = conn;
+        cmd.CommandText = "INSERT INTO data (some_field) VALUES (@p)";
+        cmd.Parameters.AddWithValue("p", "Hello world");
+        cmd.ExecuteNonQuery();
+    }
+
+    // Retrieve all rows
+    using (var cmd = new NpgsqlCommand("SELECT some_field FROM data", conn))
+    using (var reader = cmd.ExecuteReader())
+        while (reader.Read())
+            Console.WriteLine(reader.GetString(0));
+}
+     */
