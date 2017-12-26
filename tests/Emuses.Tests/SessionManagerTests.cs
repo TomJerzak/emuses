1af79976c18cf43e2e059ec1ed50aef30e4c6e64
teja-1010100 <tomasz.jerzak@gmail.com>
2017-12-26 22:29:44 +0100

    Update 'return_all_sessions' test.

--- tests/Emuses.Tests/SessionManagerTests.cs
+++ tests/Emuses.Tests/SessionManagerTests.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
 using Moq;
 using Xunit;
 
@@ -17,6 +19,12 @@ namespace Emuses.Tests
         public void return_all_sessions()
         {
             var manger = new SessionManager(_storage.Object);
+            IEnumerable<Session> sessions = new List<Session>()
+            {
+                new Session("1", "1", 1, DateTime.Now, _storage.Object),
+                new Session("2", "1", 1, DateTime.Now, _storage.Object)
+            };
+            _storage.Setup(p => p.GetAll()).Returns(sessions);
 
             manger.GetAll().Should().HaveCount(2);
         }
